7904ca75e90113417535aef81dd7876fda4e7670
nasirkhanx <p230506@pwr.nu.edu.pk>
2025-06-22 00:00:06 +0500

    random numbers

--- first_file.js
+++ first_file.js
@@ -13,3 +13,12 @@ document.getElementById("b3").onclick = function reverte(){
     no=0;
     document.getElementById("number").textContent = no;
 }
+
+/* -----------random number-------*/
+let random_no = Math.floor(Math.random()*6 + 1);//random number between 1-6
+console.log(random_no);
+ 
+let max=100;
+let min=50;
+let random_ = Math.floor(Math.random()* (max-min) + min);//random number between 1-6
+console.log(random_);
